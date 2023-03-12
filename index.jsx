f3e706ae31ea4a78f3ae57d6cd53176e31da1adc
itsownkot <127651530+itsownkot@users.noreply.github.com>
2023-03-12 20:40:00 +0700

    rename images and delete challenge comments

--- index.jsx
+++ index.jsx
@@ -6,22 +6,6 @@ import Footer from './components/Footer';
 import About from './pages/About';
 import Home from './pages/Home';
 
-/**
- * Challenge:
- * Bootstrap the VanLife project by creating the first 2 routes:
- * Home and About.
- * 
- * Also include the navbar that can link between the two routes.
- * For now, you'll either need to copy/paste the navbar code
- * to both Home and About pages, or you'll need to find a place
- * to put it where it can be shared between the two pages.
- * (Don't overthink this part - just do whatever is easiest for
- * you because we'll learn a better approach very soon)
- * 
- * Review challenge: do all the CSS yourself based on the design
- * linked in the slides.
- */
-
 function App() {
   const [chosenLink, setChosenLink] = useState('');
   
